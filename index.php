4f922175e17cbf49cae5852b82694ad4423ed828
Alexander <alex.wtho@gmail.com>
2013-04-12 22:49:19 +0800

    color

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <title>Blog Circle</title>
+    <link title="Bootstrap" href="lib/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+      
+    </style>
+    <script src="lib/js/bootstrap.min.js" type="application/x-javascript"></script>
+    <script src="http://code.jquery.com/jquery.js" type="application/x-javascript"></script>
+  </head>
+  <body>
+    <div style="margin-top: 35px;" class="container">
+      <div style="margin-top: -30px;" class="navbar  navbar-static-top">
+        <div class="navbar-inner"> <a class="brand" href="#">Title</a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="span1"> Hi </div>
+        <div class="span7"> World </div>
+      </div>
+       </div>
+  </body>
+</html>
